72f3d7ed613694a0b34f3ece8efce2247a3a5196
define) <120991965+definem@users.noreply.github.com>
2023-06-13 16:22:58 +0500

    updated

---
+++ sender.js
@@ -0,0 +1,12 @@
+let form = document.querySelector("#form");
+
+let bot = {
+  TOKEN: "",
+  chatID: "",
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let message = document.querySelector("#some_message");
+});
